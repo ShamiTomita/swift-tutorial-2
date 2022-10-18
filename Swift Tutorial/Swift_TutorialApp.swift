1a8a77032ca4277e7ce23a85f04e7feae2c88d71
shamitomita <shamitomita@gmail.com>
2022-10-17 22:51:25 -0400

    Initial Commit

---
+++ Swift Tutorial/Swift_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TutorialApp.swift
+//  Swift Tutorial
+//
+//  Created by Shami Tomita on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
